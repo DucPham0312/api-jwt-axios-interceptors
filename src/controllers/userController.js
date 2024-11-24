74f6468c80458e374e208ab9071afd9781592fb6
Pham Duc <dpham6398@gmail.com>
2024-11-24 23:41:28 +0700

    API refresh Token lam moi Access Token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -76,7 +76,10 @@ const login = async (req, res) => {
 
 const logout = async (req, res) => {
   try {
-    // Do something
+    // xóa cookie(làm ngược lại với gán cookie ở login)
+    res.clearCookie('accessToken')
+    res.clearCookie('Rrfresh')
+
     res.status(StatusCodes.OK).json({ message: 'Logout API success!' })
   } catch (error) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error)
@@ -85,10 +88,45 @@ const logout = async (req, res) => {
 
 const refreshToken = async (req, res) => {
   try {
-    // Do something
-    res.status(StatusCodes.OK).json({ message: ' Refresh Token API success.' })
+    //Cách 1: Lấy luôn từ Cookie đã đính kèm vào request
+    // const refeshTokenFromCookie = req.cookie?.refreshToken
+
+    //Cách 2: Từ LocalStorage phía FE sẽ truyền vào body khi gọi api
+    const refeshTokenFromBody = req.body?.refreshToken
+
+    //Verify/ giải mã refresh token xem có hợp lệ hay không
+    const refreshTokenDecoded = await JwtProvider.verifyToken(
+      // refeshTokenFromCookie, //cach1
+      refeshTokenFromBody, //cach2
+      REFRESH_TOKEN_SECRET_SIGNATURE
+    )
+    // console.log(refreshTokenDecoded)
+
+    //Vì chỉ lưu thông tin unique và cố định của user trong token rồi, vì vậy có thể lấy luôn từ decoded ra
+    const userInfo = {
+      id: refreshTokenDecoded.id,
+      email: refreshTokenDecoded.email
+    }
+
+    //Tạo accessToken mới
+    const accessToken = await JwtProvider.generateToken(
+      userInfo,
+      ACCESS_TOKEN_SECRET_SIGNATURE,
+      '1h'
+    )
+
+    //Res lại cookie accessToken mới cho TH sử dụng cookie
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none', //set để domain BE và FE không cùng site cx
+      maxAge: ms('14 days')
+    })
+
+    //Trả về accessToken mới cho trường hợp FE cần update lại trong LocalStorage
+    res.status(StatusCodes.OK).json({ accessToken })
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error)
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Refresh Token API failed.' })
   }
 }
 
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,23 +1,21 @@
 import { StatusCodes } from 'http-status-codes'
-import { JwtProvider, ACCESS_TOKEN_SECRET_SIGNATURE, REFRESH_TOKEN_SECRET_SIGNATURE } from '~/providers/JwtProvider'
-
+import { JwtProvider, ACCESS_TOKEN_SECRET_SIGNATURE } from '~/providers/JwtProvider'
 
 
 //Middleware này sẽ đảm nhiệm việc quan trọng: Lấy và xác thực JWT accessToken nhận được từ FE có hợp lệ hay không
 const isAuthorized = async (req, res, next) => {
   //Cách 1: Lấy accessToken nằm trong req cookie phía client - withCredentials trong file authorizeAxios và credentials trong CoRS
   const accessTokenFromCookie = req.cookies?.accessToken
-  console.log('accessTokenFromCookie', accessTokenFromCookie)
-  console.log('---')
+  // console.log('accessTokenFromCookie', accessTokenFromCookie)
+  // console.log('---')
   if (!accessTokenFromCookie) {
     res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Unauthorized! (Token not found)' })
   }
 
-
   //Cách 2: Lấy accessToken trong trường hợp phía FE lưu localStorage và gửi lên thông qua header authorization
   const accessTokenFromHeader = req.headers.authorization
-  console.log('accessTokenFromHeader', accessTokenFromHeader)
-  console.log('---')
+  // console.log('accessTokenFromHeader', accessTokenFromHeader)
+  // console.log('---')
   if (!accessTokenFromHeader) {
     res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Unauthorized! (Token not found)' })
     return
@@ -30,7 +28,7 @@ const isAuthorized = async (req, res, next) => {
       // accessTokenFromHeader.substring('Bearer '.length),
       ACCESS_TOKEN_SECRET_SIGNATURE
     )
-    console.log('accessTokenDecoded', accessTokenDecoded)
+    // console.log('accessTokenDecoded', accessTokenDecoded)
 
     //**Nếu token hợp lệ, cần lưu thông tin giải mã được vào req.jwtDecoded, để xử dụng ở phía sau
     req.JwtDecoded = accessTokenDecoded
@@ -39,7 +37,7 @@ const isAuthorized = async (req, res, next) => {
     next()
 
   } catch (error) {
-    console.log('Error from authMiddleware', error)
+    // console.log('Error from authMiddleware', error)
     // Trường hợp lỗi 01: Nếu cái accessToken bị hết hạn (expired), thì cần trả về một cái mã lỗi GONE - 410 cho phía FE biết để gọi API refreshToken
     if (error.message?.includes('jwt expired')) {
       res.status(StatusCodes.GONE).json({ message: 'Need to refresh token' })
